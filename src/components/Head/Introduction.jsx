c2bbb67b0d0960d96374d10138fa89b8be101c82
Sam Ronning <sronning@umich.edu>
2022-04-29 13:37:29 -0400

    update introduction

--- src/components/Head/Introduction.jsx
+++ src/components/Head/Introduction.jsx
@@ -3,11 +3,13 @@ import P from "../Common/P";
 const Introduction = () => {
   return (
     <P>
-      Hi! I am a recent graduate in computer science with general software
-      development and full-stack development experience. I am actively looking
-      for a full-time position. I wish to work at a tech company where I can
-      constantly improve my skills while working on a team in an innovative
-      environment.
+      Hi! I am a Software Applications Developer at Centene with general
+      software development and full-stack development experience. I enjoy
+      working in an environment where I can improve my skills alongside an
+      innovative team. I am currently leading a project to combine data science
+      tools into a unified web application. I am constantly learning new
+      technology and tools and have a curiosity for developing new solutions or
+      learning the best established solutions.
     </P>
   );
 };
